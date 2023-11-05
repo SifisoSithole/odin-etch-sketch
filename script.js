3cf6055bf01c5e15134e87ef5fd28aa6a1dd15c8
SifisoSithole <sitholesifisobrian@gmail.com>
2023-11-05 15:18:49 +0200

    Completed the ui for the desktop viewport

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let pen = document.querySelector('#pen-color');
 let penColor = pen.value;
 let background = document.querySelector('#background-color');
 let squareBackgroundColor = background.value;
+let isDrawing = false;
 let draw = true;
 let eraser = false;
 let rainbow = false;
@@ -11,7 +12,89 @@ pen.addEventListener('change', () => {
     penColor = pen.value;
 })
 
+let gridLines = document.querySelector('.grid-lines');
+gridLines.addEventListener('click', () => {
+    let gridSquares = document.querySelectorAll('.grid-squares');
 
+    if (gridLines.id === 'on') {
+        gridLines.id = 'off';
+        gridSquares.forEach((square) => {
+            square.style.border = 'none';
+        });
+    } else {
+        gridLines.id = 'on';
+        gridSquares.forEach((square) => {
+            square.style.border = '0.5px solid #202020';
+        });
+    }
+})
+
+let rainbowButton = document.querySelector('.rainbow');
+rainbowButton.addEventListener('click', () => {
+    if (rainbowButton.id === 'off'){
+        rainbowButton.id = 'on';
+        rainbow = true;
+    } else {
+        rainbowButton.id = 'off';
+        rainbow = false;
+    }
+})
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function addDrawingFunctionality(){
+    let gridSquares = document.querySelectorAll('.grid-squares');
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('mousedown', () => {
+            isDrawing = true;
+        })
+    })
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('mouseup', () => {
+            isDrawing = false;
+        })
+
+    });
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('mousemove', () => {
+            if (isDrawing){
+                if (draw){
+                    if (!rainbow){
+                        square.style.backgroundColor = penColor;
+                    } else {
+                        square.style.backgroundColor = getRandomColor();
+                    }
+                } else if (eraser){
+                    square.style.backgroundColor = squareBackgroundColor;
+                }
+            }
+        })
+    })
+
+    gridSquares.forEach((square) => {
+        square.addEventListener('click', () => {
+            if (draw){
+                if (!rainbow){
+                    square.style.backgroundColor = penColor;
+                } else {
+                    square.style.backgroundColor = getRandomColor();
+                }
+            } else if (eraser){
+                square.style.backgroundColor = squareBackgroundColor;
+            }
+        })
+    })
+}
 
 let range = document.querySelector('#grid-size');
 
@@ -33,6 +116,10 @@ function buildGrid() {
     divElement.style.width = `${squareSize}px`;
     divElement.style.height = `${squareSize}px`;
 
+    if (gridLines.id === 'off'){
+        divElement.style.border = 'none';
+    }
+
     for (let i = 0; i < totalSquares; i++){
         rowDiv.appendChild(divElement.cloneNode(true));
     }
@@ -40,9 +127,15 @@ function buildGrid() {
     for (let i = 0; i < totalSquares; i++){
         gridContainer.appendChild(rowDiv.cloneNode(true));
     }
+
+    addDrawingFunctionality();
 }
 
-buildGrid()
+buildGrid();
+
+document.getElementById('clear').addEventListener('click', () => {
+    buildGrid();
+})
 
 window.addEventListener('resize', () => {
     let totalSquares = parseInt(range.value);
@@ -85,19 +178,6 @@ eraserButton.addEventListener('click', () => {
         eraser = true;
         fillColorButton.id = 'off';
     }
-})
-
-let gridSquares = document.querySelectorAll('.grid-squares');
-gridSquares.forEach((square) => {
-    square.addEventListener('click', () => {
-        if (draw){
-            if (!rainbow){
-                square.style.backgroundColor = penColor;
-            }
-        } else if (eraser){
-            square.style.backgroundColor = squareBackgroundColor;
-        }
-    })
 })
 
 background.addEventListener('change', () => {
